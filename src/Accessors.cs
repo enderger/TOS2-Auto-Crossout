add253fb55565fb0fde5d80a8363c3894e506971
Danielle Hutzley <endergeryt@gmail.com>
2023-12-31 16:56:05 -0700

    Factor out accessors (classes)

---
+++ src/Accessors.cs
@@ -0,0 +1,74 @@
+// Patches which collect info from the game state
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+using HarmonyLib;
+using Server.Shared.Extensions;
+using Server.Shared.Info;
+using Server.Shared.State;
+
+namespace AutoCrossout.Accessors
+{
+  [HarmonyPatch(typeof(Game.Interface.HudRoleListAndGraveyardController), "Start")]
+  public class RoleListAccessor
+  {
+    public static Game.Interface.HudRoleListAndGraveyardController hrg_controller = null;
+
+    /// <summary>
+    /// Cross out a given role list item
+    /// </summary>
+    public static void crossOut(Game.Interface.RoleListItem it)
+    {
+      it.isCrossedOut = true;
+      it.ValidateCrossOut();
+    }
+
+    /// <summary>
+    /// Cross out the slot for a given role in the role list
+    /// </summary>
+    public static void crossOutA(Role role)
+    {
+      foreach (var item in hrg_controller.roleListPanel.roleListItems)
+      {
+        if (!item.isCrossedOut)
+        {
+          if (item.role == role)
+          {
+            crossOut(item);
+            return;
+          }
+          else if (item.role.IsBucket())
+          {
+            SharedRoleData.roleBucketLookup.TryGetValue(item.role, out var bucket);
+            if (bucket.roles.Contains(role))
+            {
+              crossOut(item);
+              return;
+            }
+          }
+        }
+      }
+    }
+
+    [HarmonyPostfix]
+    public static void Postfix(Game.Interface.HudRoleListAndGraveyardController __instance)
+    {
+      hrg_controller = __instance;
+    }
+  }
+
+  [HarmonyPatch(typeof(Game.Interface.RoleCardPanel), "HandleOnMyIdentityChanged")]
+  public class PlayerRoleInfoAccessor
+  {
+    public static int? position = null;
+    public static Role? role = null;
+
+    [HarmonyPostfix]
+    public static void Postfix(PlayerIdentityData playerIdentityData)
+    {
+      position = playerIdentityData.position;
+      role = playerIdentityData.role;
+    }
+  }
+}
--- src/Patches.cs
+++ src/Patches.cs
@@ -1,75 +1,14 @@
-// Patch which crosses out known roles in the list
+// Patches which give the mod functionality
 //
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 using HarmonyLib;
-using Server.Shared.Extensions;
 using Server.Shared.Info;
 using Server.Shared.State;
 
 namespace AutoCrossout.Patches
 {
-  [HarmonyPatch(typeof(Game.Interface.HudRoleListAndGraveyardController), "Start")]
-  public class RoleListAccessor
-  {
-    public static Game.Interface.HudRoleListAndGraveyardController hrg_controller = null;
-
-    public static void crossOut(Game.Interface.RoleListItem it)
-    {
-      it.isCrossedOut = true;
-      it.ValidateCrossOut();
-    }
-
-    public static int? crossOutA(Role role)
-    {
-      for (int i = 0; i < hrg_controller.roleListPanel.roleListItems.Count; i++)
-      {
-        var item = hrg_controller.roleListPanel.roleListItems[i];
-        if (!item.isCrossedOut)
-        {
-          if (item.role == role)
-          {
-            crossOut(item);
-            return i;
-          }
-          else if (item.role.IsBucket())
-          {
-            SharedRoleData.roleBucketLookup.TryGetValue(item.role, out var bucket);
-            if (bucket.roles.Contains(role))
-            {
-              crossOut(item);
-              return i;
-            }
-          }
-        }
-      }
-
-      return null;
-    }
-
-    [HarmonyPostfix]
-    public static void Postfix(Game.Interface.HudRoleListAndGraveyardController __instance)
-    {
-      hrg_controller = __instance;
-    }
-  }
-
-  [HarmonyPatch(typeof(Game.Interface.RoleCardPanel), "HandleOnMyIdentityChanged")]
-  public class PlayerRoleInfoAccessor
-  {
-    public static int? position = null;
-    public static Role? role = null;
-
-    [HarmonyPostfix]
-    public static void Postfix(PlayerIdentityData playerIdentityData)
-    {
-      Utils.WriteLog("Writing current player role info (player position: " + playerIdentityData.position + ")");
-      position = playerIdentityData.position;
-      role = playerIdentityData.role;
-    }
-  }
-
   [HarmonyPatch(typeof(Game.Interface.HudGraveyardPanel), "CreateGraveyardItem")]
   public class CrossOutDeadRoles
   {
@@ -78,22 +17,22 @@ namespace AutoCrossout.Patches
     {
       Server.Shared.State.Role roleKilled = killRecord.playerRole;
 
-      if (killRecord.playerId != PlayerRoleInfoAccessor.position)
+      if (killRecord.playerId != Accessors.PlayerRoleInfoAccessor.position)
       {
         Utils.WriteLog("Crossing out dead player's role : " + roleKilled);
-        RoleListAccessor.crossOutA(roleKilled);
+        Accessors.RoleListAccessor.crossOutA(roleKilled);
       }
     }
   }
-  
-  public class CrossOutPlayerRole
+
+  public static class CrossOutPlayerRole
   {
-    public static void HandlePostfix(GameInfo gameInfo)
+    internal static void HandlePostfix(GameInfo gameInfo)
     {
-      if (gameInfo.playPhase == PlayPhase.FIRST_DISCUSSION && PlayerRoleInfoAccessor.role.HasValue)
+      if (gameInfo.playPhase == PlayPhase.FIRST_DISCUSSION && Accessors.PlayerRoleInfoAccessor.role.HasValue)
       {
-        Utils.WriteLog("Crossing out current player's role : " + PlayerRoleInfoAccessor.role);
-        RoleListAccessor.crossOutA(PlayerRoleInfoAccessor.role.Value);
+        Utils.WriteLog("Crossing out current player's role : " + Accessors.PlayerRoleInfoAccessor.role);
+        Accessors.RoleListAccessor.crossOutA(Accessors.PlayerRoleInfoAccessor.role.Value);
       }
     }
 
