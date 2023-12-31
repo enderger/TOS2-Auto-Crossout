99bca19ee53ec156d36dd70cef2d63ea48531926
Danielle Hutzley <endergeryt@gmail.com>
2023-12-31 01:32:25 -0700

    Add LICENSE

--- src/ModInfo.fs
+++ src/ModInfo.fs
@@ -1,4 +1,8 @@
 // Shared mod info for AutoCrossout
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
 module AutoCrossout.ModInfo
 
 let PLUGIN_GUID = "AutoCrossout"
--- src/Utils.fs
+++ src/Utils.fs
@@ -1,4 +1,8 @@
 // Shared utilities for AutoCrossout
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
 module AutoCrossout.Utils
 
 let write_log message: unit = System.Console.WriteLine (sprintf "[%s] %s" AutoCrossout.ModInfo.PLUGIN_GUID message)
